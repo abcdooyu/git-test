d17c4f8c4eae40746e6407c2d4dd78c2e12773db
shawnchan <abcdooyu@163.com>
2020-08-29 15:39:51 +0800

    把本地工程推送到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,20 @@
+package com.itheima.git;
+
+/**
+ * @program: git-test-project
+ * @description:
+ * @author: Mr.Chan
+ * @create: 2020-08-29 15:31
+ **/
+public class FirstClass {
+    public static void main(String[] args){
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
